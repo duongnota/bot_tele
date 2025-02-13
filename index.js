a71c39630be85d8ddb895c8d8bb40ee5b6da0356
duongnota <duongnn@notavn.com>
2025-02-13 11:01:12 +0700

    log req

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.post("/", (req, res) => {
      console.log("ehehe deployed + sended");
-     console.log(req.payload);
+     console.log(req);
      res.send("Hello World!").status(200);
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
