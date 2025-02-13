8edfe447cd557eeb61fc9d50d4b7cfcc13fafa5b
duong <duongnn@notavn.com>
2025-02-13 10:39:26 +0700

    add status

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.push("/", (req, res) => res.send("Hello World!"));
+app.push("/", (req, res) => res.send("Hello World!").status(200));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
