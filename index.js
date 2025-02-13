ef5f3cc80e442e411ef4c998e0bb5bf72c633bc5
duong <duongnn@notavn.com>
2025-02-13 10:43:53 +0700

    asdas

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.post("/", (req, res) => res.send("Hello World!").status(200));
+app.post("/", (req, res) => {
+     console.log("ehehe deployed + sended");
+     res.send("Hello World!").status(200);
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
