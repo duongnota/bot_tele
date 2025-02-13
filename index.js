d26936af6b895f3f40ab6a503728d6ee9347fd87
duong <duongnn@notavn.com>
2025-02-13 11:21:49 +0700

    asd

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
 app.post("/", (req, res) => {
-     console.log("version log req");
-     console.log(req);
+     console.log("Received Payload:", req.body);
      res.send("Hello World!").status(200);
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
